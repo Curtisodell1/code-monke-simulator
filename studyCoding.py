447dab4e55b2fc78ae2c49d3d08a3f95eb668c91
Curtis O'Dell <curtisodell1@gmail.com>
2023-09-04 13:18:42 -0600

    added basic options, incrementer for coder cred score

--- studyCoding.py
+++ studyCoding.py
@@ -1,11 +1,12 @@
-
 study_loop = True
 while(study_loop):
 
     print("How will you study?:")
-    print("1. Study Coding")
-    print('2. Increase Coder Cred')
-    print("x. Exit the program")
+    # these options will be replaced by a SQL database of topics to study
+    # users can either query the database to choose a topic or get something new
+    print("1. Study Option 1")
+    print("2. Study Option 2")
+    print("x. Return to main menu")
     command = input("Input your command here:")
 
     if command == "1":
