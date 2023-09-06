a69ddf643b354cb5c46aee4f6ebdabe1b6acbd34
Curtis O'Dell <curtisodell1@gmail.com>
2023-09-06 16:29:51 -0600

    working study code main

--- lib/main.py
+++ lib/main.py
@@ -4,17 +4,26 @@ actions = 0
 
 while (looping):
     #main menu
+    if actions > 2:
+        actions = 0
+        days += 1
+        from cliArt import header_art
+        header_art()
+        print("Good job code monkey! You've made it through another day of Googling and using CTRL + C and CTRL + V")
+        from cliArt import footer_art
+        footer_art()
     print("Main Menu:")
     print("1. Study Coding")
     print('2. Increase Coder Cred')
     print("x. Exit the program")
-
     command = input("Input your command here:")
     if command == "1": 
+        actions += 1
         with open("lib/studyCoding.py") as study_coding:
             study_coding_loop = study_coding.read()
         exec(study_coding_loop)
     elif command == "2":
+        actions += 1
         with open("lib/coderCred.py") as coder_cred:
             coder_cred_loop = coder_cred.read()
         exec(coder_cred_loop)
